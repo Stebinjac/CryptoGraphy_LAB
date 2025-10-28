fe22251d4356c98db547fabceeb530d743ae246e
Stebin Jacob Titus <161051682+Stebinjac@users.noreply.github.com>
2025-10-28 12:25:40 +0530

    Add files via upload

---
+++ RC4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import javax.crypto.*;
+public class des{
+    public static void main(String[]args)throws Exception{
+        System.out.println("enter plaintext");
+        Scanner sc=new Scanner(System.in);
+        String plaintext=sc.nextLine();
+        KeyGenerator key=KeyGenerator.getInstance("ARCFOUR");
+        SecretKey sk=key.generateKey();
+        Cipher cipher=Cipher.getInstance("ARCFOUR");
+        cipher.init(Cipher.ENCRYPT_MODE,sk);
+        byte[] encbyte=cipher.doFinal(plaintext.getBytes());
+        String enctext=Base64.getEncoder().encodeToString(encbyte);
+        System.out.println("encrytped text :"+enctext);
+        cipher.init(Cipher.DECRYPT_MODE,sk);
+        byte[] decbyte=cipher.doFinal(encbyte);
+        String dectext=new String(decbyte);
+        System.out.println("decrypted text :"+dectext);
+    }
+}
